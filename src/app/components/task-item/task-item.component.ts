67406bd8d30cffc95e88e95a7f1003969c2937b5
SYSMX\erosenhart <erosenhart@contactmakers.nl>
2023-10-05 18:26:26 +0100

    my first Observable

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Task } from 'src/app/Task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css']
+})
+export class TaskItemComponent implements OnInit {
+  @Input() task: Task;
+  faTimes = faTimes;
+
+  constructor() {
+    this.task = {
+      id: 0,
+      text: '',
+      day: '',
+      reminder: false,
+    }
+  }
+  
+  ngOnInit(): void { }
+}
